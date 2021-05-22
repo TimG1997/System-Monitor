0dbbeb50eb049ff740d6a6766e62fe9cf723d741
Tim Grein <tim@4greins.de>
2021-05-22 15:01:16 +0200

    Add default parameters to be able to test methods with custom files.

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -5,8 +5,9 @@
 #include <regex>
 #include <string>
 
-namespace LinuxParser {
-// Paths
+#include "linux_info_file_locations.h"
+#include "cpu_helper.h"
+
 const std::string kProcDirectory{"/proc/"};
 const std::string kCmdlineFilename{"/cmdline"};
 const std::string kCpuinfoFilename{"/cpuinfo"};
@@ -18,14 +19,15 @@ const std::string kVersionFilename{"/version"};
 const std::string kOSPath{"/etc/os-release"};
 const std::string kPasswordPath{"/etc/passwd"};
 
+namespace LinuxParser {
 // System
-float MemoryUtilization();
-long UpTime();
+float MemoryUtilization(std::string mem_info_path = LinuxInfoFileLocations::kProcDirectory + kMeminfoFilename);
+long UpTime(std::string uptime_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kUptimeFilename);
 std::vector<int> Pids();
-int TotalProcesses();
-int RunningProcesses();
-std::string OperatingSystem();
-std::string Kernel();
+int TotalProcesses(std::string stat_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kStatFilename);
+int RunningProcesses(std::string stat_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kStatFilename);
+std::string OperatingSystem(std::string os_path = LinuxInfoFileLocations::kOSPath);
+std::string Kernel(std::string kernel_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kVersionFilename);
 
 // CPU
 enum CPUStates {
@@ -40,18 +42,19 @@ enum CPUStates {
   kGuest_,
   kGuestNice_
 };
-std::vector<std::string> CpuUtilization();
-long Jiffies();
-long ActiveJiffies();
-long ActiveJiffies(int pid);
-long IdleJiffies();
+std::vector<std::string> CpuUtilization(std::vector<int> pids = Pids(), std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string stat_path =LinuxInfoFileLocations::kStatFilename, std::string uptime_path = LinuxInfoFileLocations::kUptimeFilename, long clock_ticks_per_second = CpuHelper::ClockTicksPerSecond());
+long Jiffies(std::string stat_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kStatFilename);
+long ActiveJiffies(std::string stat_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kStatFilename);
+long IdleJiffies(std::string stat_path = LinuxInfoFileLocations::kProcDirectory + LinuxInfoFileLocations::kStatFilename);
 
 // Processes
-std::string Command(int pid);
-std::string Ram(int pid);
-std::string Uid(int pid);
+long ActiveJiffies(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string stat_filename = LinuxInfoFileLocations::kStatFilename);
+std::string Command(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string command_filename = LinuxInfoFileLocations::kCmdlineFilename);
+std::string Ram(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string status_filename = LinuxInfoFileLocations::kStatusFilename);
+std::string Uid(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string command_filename = LinuxInfoFileLocations::kStatusFilename);
 std::string User(int pid);
-long int UpTime(int pid);
+long int UpTime(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string uptime_path = LinuxInfoFileLocations::kUptimeFilename, std::string command_filename = LinuxInfoFileLocations::kStatFilename);
+long int StartTime(int pid, std::string proc_dir = LinuxInfoFileLocations::kProcDirectory, std::string command_filename = LinuxInfoFileLocations::kStatFilename);
 };  // namespace LinuxParser
 
 #endif
