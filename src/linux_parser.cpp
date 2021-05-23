3e318c0e2492a529eb9bdc05cc2b31f6f64d4581
Tim Grein <tim@4greins.de>
2021-05-23 19:57:04 +0200

    Refactor ActiveJiffies in linux_parser.cpp to use the built in CPU states.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -199,18 +199,31 @@ long LinuxParser::ActiveJiffies(int pid) {
 }
 
 long LinuxParser::ActiveJiffies() {
-  string cpu, user, nice, system, idle, iowait, irq, softirq, steal, guest,
-      guest_nice;
   string line;
   std::ifstream stream(kProcDirectory + kStatFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
-    std::istringstream linestream(line);
-    linestream >> cpu >> user >> nice >> system >> idle >> iowait >> irq >>
-        softirq >> steal >> guest >> guest_nice;
 
-    return stol(user) + stol(nice) + stol(system) + stol(iowait) + stol(irq) +
-           stol(softirq) + stol(steal) + stol(guest) + stol(guest_nice);
+    int skip_first_element = 1;
+    long cpu_jiffies_sum = 0;
+    vector<long> cpu_jiffies = StringHelper::GetElements<long>(
+        line, vector<int>{
+            CPUStates::kUser_,
+            CPUStates::kNice_,
+            CPUStates::kSystem_,
+            CPUStates::kIOwait_,
+            CPUStates::kIRQ_,
+            CPUStates::kSoftIRQ_,
+            CPUStates::kSteal_,
+            CPUStates::kGuest_,
+            CPUStates::kGuestNice_,
+        }, skip_first_element);
+
+    for(auto &cpu_jiffie : cpu_jiffies){
+      cpu_jiffies_sum += cpu_jiffie;
+    }
+
+    return cpu_jiffies_sum;
   }
 
   return 0;
